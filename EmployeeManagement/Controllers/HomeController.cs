362d83f0509c86e8cfdc41fc4cdc7145c77ccb45
Colin Lancaster <lancaster.colin@gmail.com>
2020-08-26 09:18:01 -0700

    initial commit

---
+++ EmployeeManagement/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using EmployeeManagement.Models;
+using EmployeeManagement.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement.Controllers
+{
+    //[Route("[controller]/[action]")]
+    public class HomeController : Controller
+    {
+        private IEmployeeRepository _employeeRepository;
+
+        // Injected via the constructor
+        public HomeController(IEmployeeRepository employeeRepository)
+        {
+            _employeeRepository = employeeRepository;
+        }
+        
+        //[Route("")]
+        // using tokens like "action" is powerful
+        // It gives you flexibility to change the name of your route,
+        // without worrying about updating the route attribute value.
+        //[Route("[action]")]
+        //[Route("~/")]
+        public ViewResult Index()
+        {
+            var model = _employeeRepository.GetAllEmployees();
+            return View(model);
+        }
+
+        //[Route("{id?}")]
+        public ViewResult Details(int? id)
+        {
+            HomeDetailsViewModel homeDetailsViewModel = new HomeDetailsViewModel()
+            {
+                // Null Coalescing Operator.
+                // If the incoming param is not null, use id.
+                // If the incoming param is null, use 1.
+                Employee = _employeeRepository.GetEmployee(id??1),
+                PageTitle = "Employee Details",
+            };
+
+            return View(homeDetailsViewModel);
+        }
+    }
+}
---
+++ EmployeeManagement/Models/MockEmployeeRepository.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement.Models
+{
+    public class MockEmployeeRepository : IEmployeeRepository
+    {
+        private List<Employee> _employeeList;
+
+        public MockEmployeeRepository()
+        {
+            _employeeList = new List<Employee>()
+            {
+                new Employee() { Id = 1, Name = "Mary", Department = "HR", Email = "Mary@Thing.com" },
+                new Employee() { Id = 2, Name = "John", Department = "Sales", Email = "John@Thing.com" },
+                new Employee() { Id = 3, Name = "Sam", Department = "IT", Email = "Sam@Thing.com" }
+            };
+        }
+
+        public IEnumerable<Employee> GetAllEmployees()
+        {
+            return _employeeList;
+        }
+
+        public Employee GetEmployee(int Id)
+        {
+            return _employeeList.FirstOrDefault(e => e.Id == Id);
+        }
+
+        
+    }
+}
