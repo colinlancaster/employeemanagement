95ba6937e4cbb795dffab48f1780b273ed54d531
Colin Lancaster <Colinl@biz.oregon.gov>
2020-08-27 12:34:13 -0700

    started migration section

---
+++ EmployeeManagement/Migrations/20200827192813_AlteredSeedDataAgain.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EmployeeManagement.Migrations
+{
+    public partial class AlteredSeedDataAgain : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Employees",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "Email",
+                value: "colin@pragim.co");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Employees",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "Email",
+                value: "john@pragim.co");
+        }
+    }
+}
---
+++ EmployeeManagement/Models/ModelBuilderExtensions.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement.Models
+{
+    public static class ModelBuilderExtensions
+    {
+        public static void Seed(this ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Employee>().HasData(
+                new Employee
+                {
+                    Id = 1,
+                    Name = "Mary",
+                    Department = Dept.HR,
+                    Email = "mary@pragim.co"
+                },
+                new Employee
+                {
+                    Id = 2,
+                    Name = "Colin",
+                    Department = Dept.IT,
+                    Email = "colin@pragim.co"
+                }
+            );
+        }
+    }
+}
